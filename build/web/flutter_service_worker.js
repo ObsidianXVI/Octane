e338671328e0502eeb3bdb1f2114e5059c73c05d
ObsidianXVI <siddharth.chitikela@gmail.com>
2023-05-25 21:59:46 +0800

    this better work

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "c4cb76154eeb88f76fc676303fae92bb",
-"index.html": "c4bfae29e050e26a618e9b87e9f7d205",
-"/": "c4bfae29e050e26a618e9b87e9f7d205",
+"index.html": "0fb4f2887d00fe75cfa553b11176167d",
+"/": "0fb4f2887d00fe75cfa553b11176167d",
 "main.dart.js": "33b1955f2fa586b0e9cdcd540156661a",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
