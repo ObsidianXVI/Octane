eedca5e550c61283d23cead533dfb202b9c70fee
ObsidianXVI <siddharth.chitikela@gmail.com>
2023-07-16 15:41:46 +0800

    build and deploy to firebase

--- lib/letterpress/components/lp_gallery.dart
+++
@@ -1,1 +0,0 @@
-part of octane.letterpress.ds;
--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:octane/letterpress/views/lp_views.dart';
 import 'package:octane/octane_ds/octane_ds.dart';
 import './views/views.dart';
 
 void main() {
-  runApp(Octane(
-    routables: [
-      LPRouter.home(),
-      LPRouter.tgif('week1-turbocal-complex-calendar-widget-flutter'),
-    ],
-  ));
+  runApp(const OctaneApp());
 }
 
-class Octane extends StatelessWidget {
-  final List<Routable> routables;
-  final Map<String, Widget Function(BuildContext)> routes;
-  Octane({required this.routables, super.key})
-      : routes = <String, Widget Function(BuildContext)>{
-          for (Routable r in routables) r.toSlug(): r.buildFn
-        };
+class OctaneApp extends StatelessWidget {
+  const OctaneApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const Material(
+      home: Material(
         child: OCTVLaunchView(),
       ),
-      routes: routes,
-      /* {
-        '/letterpress': (context) => const Material(
-              child: OCTVLetterpressHomeView(),
-            ),
-        '/letterpress/home': (context) => ,
-        /* '/letterpress/tgif-challenge/week1-turbocal': (context) => Material(
-              child: OCTVLetterpressPostView(
-                postComponents:
-                    lp_store.LetterpressTGIFWeek1TurbocalPost.postComponents,
-              ),
-            ), */
-        '/letterpress/tgif/': (context) => Material(
-              child: OCTVLetterpressPostView(
-                lpPost: LPPost(
-                  postConfigs: LPPostConfigs(
-                      title: 'Complex Calendar Widget In Flutter',
-                      postClass: LPPostClass.tgif_challenge,
-                      publicationDate: DateTime.now(),
-                      lastUpdate: DateTime.now().add(const Duration(days: 2))),
-                  components: [],
-                ),
-              ),
-            )
-      } */
+      routes: {},
     );
   }
 }
